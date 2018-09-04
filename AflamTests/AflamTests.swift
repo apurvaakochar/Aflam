5e655f2ddf0f28f0925e5d69545c9d20e821b6d8
Apurva Kochar <apurvaakochar@gmail.com>
2018-09-04 13:23:40 +0200

    Unit Tests

--- AflamTests/AflamTests.swift
+++ AflamTests/AflamTests.swift
@@ -7,29 +7,66 @@
 //
 
 import XCTest
+import RealmSwift
+
 @testable import Aflam
 
 class AflamTests: XCTestCase {
     
+    var movieUnderTest: Movie!
+    var searchResultsUnderTest: SearchResultsInteractor!
+    var serverRequestUnderTest: ServerRequest!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        serverRequestUnderTest = ServerRequest()
+        searchResultsUnderTest = SearchResultsInteractor()
+        
+        let dictionary = ["poster_path": "", "release_date": "1970-12-12", "title": "Aladdin", "overview" : "It is a fanatstic movie!"]
+        movieUnderTest = Movie(dictionary)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        movieUnderTest = nil
+        serverRequestUnderTest = nil
+        searchResultsUnderTest = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMovieAttributes() {
+        XCTAssertEqual(movieUnderTest.overview, "It is a fanatstic movie!")
+        XCTAssertEqual(movieUnderTest.date, "1970-12-12")
+        XCTAssertEqual(movieUnderTest.name, "Aladdin")
+        XCTAssertEqual(movieUnderTest.posterUrl, URL(string: "http://image.tmdb.org/t/p/w500"))
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // MARK: - Async Task
+    
+    func testGetResponse() {
+        let path = Path.API
+        let parameters = ["api_key" : Path.API_KEY, "query" : "Alladin", "page" : "1"]
+        
+        let promise = expectation(description: "Success")
+        serverRequestUnderTest.getRequest(path: path, parameters: parameters) {
+            apiResult in
+            if apiResult != nil {
+                promise.fulfill()
+            }
+            else {
+                XCTFail("Failed")
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testDataRetreivalFromRealm() {
+        let realm = try! Realm()
+        searchResultsUnderTest.getLastSearches()
+        if let count = realm.objects(SearchList.self).first?.list.count{
+            XCTAssertEqual(searchResultsUnderTest.numberOfRows, count)
+        }
+        else {
+            XCTAssertEqual(searchResultsUnderTest.numberOfRows, 0)
         }
     }
     
